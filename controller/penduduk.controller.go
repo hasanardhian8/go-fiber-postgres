3f9111169cefe5d31109a2a660a6c89cb330be56
hasanardhian8 <hasanardhian11@gmail.com>
2024-03-03 08:57:09 +0700

    revisi controller penduduk

--- controller/penduduk.controller.go
+++ controller/penduduk.controller.go
@@ -2,40 +2,43 @@ package controller
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/hasanardhian8/go-fiber-postgres/config"
 	"github.com/hasanardhian8/go-fiber-postgres/models"
-	"github.com/jinzhu/gorm"
 )
 
+type inputPen struct {
+	Nama string `json:"nama"`
+	Rt   string `json:"rt"`
+}
+
 func GetPenduduk(c *fiber.Ctx) error {
-	db := c.Locals("db").(*gorm.DB)
 	var pen []models.Penduduks
 
-	db.Find(&pen)
+	config.DBConn.Find(&pen)
 	return c.Status(fiber.StatusOK).JSON(&pen)
 }
 
 func CreatePenduduk(c *fiber.Ctx) error {
-	db := c.Locals("db").(*gorm.DB)
-	var inputPen models.Penduduks
-
-	if err := c.BodyParser(&inputPen); err != nil {
+	body := inputPen{}
+	if err := c.BodyParser(&body); err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
+	var pen models.Penduduks
+	pen.Nama = body.Nama
+	pen.Rt = body.Rt
 
-	db.Create(&inputPen)
-	return c.Status(fiber.StatusCreated).JSON(&inputPen)
+	if result := config.DBConn.Create(&pen); result.Error != nil {
+		return fiber.NewError(fiber.StatusNotFound, result.Error.Error())
+	}
+	return c.Status(fiber.StatusCreated).JSON(&pen)
 }
 
 func GetPendudukById(c *fiber.Ctx) error {
 	id := c.Params("id")
-	db := c.Locals("db").(*gorm.DB)
 	var pen models.Penduduks
 
-	if err := db.Where("id = ?", id).First(&pen).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return c.Status(fiber.StatusNotFound).SendString("Penduduk not found")
-		}
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	if result := config.DBConn.First(&pen, id); result.Error != nil {
+		return fiber.NewError(fiber.StatusNotFound, result.Error.Error())
 	}
 
 	return c.Status(fiber.StatusOK).JSON(&pen)
@@ -43,38 +46,34 @@ func GetPendudukById(c *fiber.Ctx) error {
 
 func UpdatePenduduk(c *fiber.Ctx) error {
 	id := c.Params("id")
-	db := c.Locals("db").(*gorm.DB)
+	updatebody := inputPen{}
+
+	if err := c.BodyParser(&updatebody); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
 	var pen models.Penduduks
 
-	if err := db.Where("id = ?", id).First(&pen).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return c.Status(fiber.StatusNotFound).SendString("Penduduk not found")
-		}
-		return err
+	if result := config.DBConn.First(&pen, id); result.Error != nil {
+		return fiber.NewError(fiber.StatusNotFound, result.Error.Error())
 	}
 
-	if err := c.BodyParser(&pen); err != nil {
-		return err
-	}
+	pen.Nama = updatebody.Nama
+	pen.Rt = updatebody.Rt
 
-	db.Save(&pen)
+	config.DBConn.Save(&pen)
 
 	return c.Status(fiber.StatusOK).SendString("Penduduk updated")
 }
 
 func DeletePenduduk(c *fiber.Ctx) error {
 	id := c.Params("id")
-	db := c.Locals("db").(*gorm.DB)
 	var pen []models.Penduduks
 
-	if err := db.Where("id = ?", id).First(&pen).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return c.Status(fiber.StatusNotFound).SendString("Penduduk not found")
-		}
-		return err
+	if result := config.DBConn.First(&pen, id); result.Error != nil {
+		return fiber.NewError(fiber.StatusNotFound, result.Error.Error())
 	}
 
-	db.Delete(&pen)
+	config.DBConn.Delete(&pen)
 
 	return c.Status(fiber.StatusOK).SendString("Penduduk deleted")
 }
