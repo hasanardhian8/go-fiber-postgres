be831ecb56e7325144f46491ac52d3a6a61c3c6e
hasanardhian8 <hasanardhian11@gmail.com>
2024-03-03 09:52:13 +0700

    crud done

--- models/bayar.models.go
+++ models/bayar.models.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Bayars struct {
-	Id         int       `json:"id" gorm:"primary_key`
+	Id         int       `json:"id" gorm:"primary_key"`
 	Idpenduduk int       `json:"idPenduduk" gorm:"Foreignkey:PenduduksRefer; AssociationForeignKey:Id"`
 	Idpetugas  int       `json:"idPetugas" gorm:"Foreignkey:PetugasesRefer; AssociationForeignKey:Id"`
 	Nominal    int       `json:"nominal"`
--- models/petugas.models.go
+++ models/petugas.models.go
@@ -1,7 +1,7 @@
 package models
 
 type Petugases struct {
-	Id       int    `json:"id" gorm:"primary_key`
+	Id       int    `json:"id" gorm:"primary_key"`
 	Nama     string `json:"nama" `
 	Password string `json:"password" gorm:"not null;"`
 	Role     string `json:"role"`
